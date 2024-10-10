a7613b2891c216aefeeefd07d7a8c7ad601390d4
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-10 14:29:09 +0200

    Primer comit(Contar2)

---
+++ actividade2.5.4/src/contar2/Contar2.java
@@ -0,0 +1,18 @@
+package contar2;
+
+/**
+ * Este programa mostra por pantalla os 10 primeiros numeros(0-9) usando un bucle do while
+ * @author Xaquin Alves Gonzalez
+ */
+public class Contar2 {
+
+    public static void main(String[] args) {
+        int number = 0;
+        
+        do{
+            System.out.println(number);
+            number ++;
+        }while(number < 10);
+    }
+    
+}
