76cf69657b3ec38544fc34c28f07591adbea366a
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-10 14:27:55 +0200

    Primer comit(Contar)

---
+++ actividade2.5.3/src/contar/Contar.java
@@ -0,0 +1,17 @@
+package contar;
+
+/**
+ * Este programa mostra por pantalla os 10 primeiros numeros(0-9) usando un bucle for
+ * @author Xaquin Alves Gonzalez
+ */
+public class Contar {
+
+    public static void main(String[] args) {
+
+        for(int i=0;i < 10;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
+
