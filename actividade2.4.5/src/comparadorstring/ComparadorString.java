c94aaf53afec587c40f40cf004872e78aa455dc3
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-10 14:23:44 +0200

    Primer comit(ComparadorString)

---
+++ actividade2.4.5/src/comparadorstring/ComparadorString.java
@@ -0,0 +1,16 @@
+package comparadorstring;
+
+/**
+ * Este programa manexa a clase StringComparator creada por nos
+ * @author Xaquin Alves González
+ */
+public class ComparadorString {
+
+    public static void main(String[] args) {
+        //Creamos unha nova instancia da clase StringComparator
+        StringComparator comparador = new StringComparator("Hola que tal", "Buenas tardes", "Buenos dias");
+        //Mostramos por pantalla o método getBigger() para comprobar o funcionamento
+        System.out.println("O texto máis largo é: "+comparador.getBigger());
+    }
+    
+}
---
+++ actividade2.4.5/src/comparadorstring/StringComparator.java
@@ -0,0 +1,41 @@
+package comparadorstring;
+
+/**
+ * Esta é a clase StringComparator, que posee tres atributos tipo String
+ * @author Xaquin Alves González
+ */
+public class StringComparator {
+    //Creanse as tres variables String coas que traballa esta clase 
+    private String chain1;
+    private String chain2;
+    private String chain3;
+    
+    /** Constructor da clase que recibe 3 parametros: 
+     @param chain1 unha cadea de texto calquera
+     @param chain2 unha cadea de texto calquera
+     @param chain3 unha cadea de texto calquera */
+    public StringComparator(String chain1, String chain2, String chain3) {
+        this.chain1 = chain1;
+        this.chain2 = chain2;
+        this.chain3 = chain3;
+    }
+    
+    /**Este método compara a lonxitude dos String valendose do metodo length()
+     * da clase String e unha sentencia if else
+     * @return O String de maior lonxitude dos 3 almacenados */
+    public String getBigger(){
+        if(chain1.length() > chain2.length() && chain1.length() > chain3.length()){
+            return chain1;
+        }
+        else if(chain2.length() > chain1.length() && chain2.length() > chain3.length()){
+            return chain2;
+        }
+        else if(chain3.length() > chain1.length() && chain3.length() > chain2.length()){
+            return chain3;
+        }
+        else{
+            System.out.println("Hay dous textos de igual lonxitude");
+            return null;
+        }
+    }
+}
