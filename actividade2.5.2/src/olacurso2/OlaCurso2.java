6698a2c8695ba185a08e06185fe62456f6889931
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-10 14:26:46 +0200

    Primer comit(olaCurso2)

---
+++ actividade2.5.2/src/olacurso2/OlaCurso2.java
@@ -0,0 +1,16 @@
+package olacurso2;
+/**
+ * Este programa mostra por pantalla "Ola Curso!" 7 veces usando un bucle for
+ * @author Xaquin Alves Gonzalez
+ */
+public class OlaCurso2 {
+    
+    //Facemos un bucle for que empece co contador en 0 e acabe en 6, o que da 7 iteracions
+    public static void main(String[] args) {
+        for(int i=0; i<7; i++){
+            System.out.println("Ola curso!");
+        }
+    }
+    
+}
+
