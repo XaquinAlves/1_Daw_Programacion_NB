8331b83f5d98f0808b8303ab21197a9aa6ade3d7
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-14 09:24:46 +0200

    First comit(Impares2)

---
+++ actividade2.6.1/src/impares2/Impares2.java
@@ -0,0 +1,22 @@
+package impares2;
+
+/**
+ *Este programa mostra por pantalla os 10 primeiros impares usando un bucle
+ * do while e unha sentencia if
+ * @author Xaquin Alves Gonzalez
+ */
+public class Impares2 {
+
+    public static void main(String[] args) {
+        int number = 0, i = 0;
+
+        do {
+            if (number % 2 != 0) {
+                System.out.println(number);
+                i++;
+            }
+            number --;
+        } while (i < 10);
+    }
+
+}
