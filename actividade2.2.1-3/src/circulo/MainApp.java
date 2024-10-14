694ffc29a16f1a355f9784290d320b7b300156ac
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-14 09:46:32 +0200

    Second comit(Circulo)

--- actividade2.2.1-3/src/circulo/MainApp.java
+++ actividade2.2.1-3/src/circulo/MainApp.java
@@ -8,7 +8,7 @@ public class MainApp {
 
     public static void main(String[] args) {
         //Mostramos o limite establecido para os radios
-        System.out.println("O radio máximo é "+Circle.getMAX_RADIUS());
+        System.out.println("O radio máximo é "+ Circle.getMAX_RADIUS());
         //Creamos os dous circulos dandolle o valor do radio
         Circle circle1 = new Circle(3);
         Circle circle2 = new Circle(9);
