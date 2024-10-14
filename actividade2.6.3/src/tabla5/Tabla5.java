0d503631d01f976b157c2fd8791d3a74869a903d
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-14 09:28:13 +0200

    First comit(TablaDel5)

---
+++ actividade2.6.3/src/tabla5/Tabla5.java
@@ -0,0 +1,18 @@
+package tabla5;
+
+/**
+ * Este programa mostra por pantalla a tabla de multiplicar do 5, facendo uso
+ * dun bucle for
+ *
+ * @author Xaquin Alves Gonzalez
+ */
+public class Tabla5 {
+
+    public static void main(String[] args) {
+        for(int i=0;i <11;i++){
+            System.out.println("5 x "+i+" = "+i*5);
+        }
+    }
+
+}
+
