4ded7e3d48e4a54928971672b106f64f420d9a22
XaquinAlves <xaquinalves@gmail.com>
2024-10-17 12:57:24 +0200

    Third commmit(SumaMultiplos5v3)

--- actividade2.7.1-3/src/multiplos5/Multiplos5.java
+++ actividade2.7.1-3/src/multiplos5/Multiplos5.java
@@ -9,20 +9,20 @@ package multiplos5;
 public class Multiplos5 {
 
     public static void main(String[] args) {
-        //Creanse duas variables int para gardar os numeros e unha para o resultado
+        //Crea dous int para gardar os numeros e un para o resultado
         int number1, number2, result = 0;
         number1 = 33;
         number2 = 7;
-        //Crease unha sentencia if que comproba se o primeiro numero e menor
+        //Comproba se o primeiro numero e menor
         if (number1 > number2) {
-            //Crease unha variable int para facer o intercambio de variables
+            //Crea un int para facer o intercambio de variables
             int tempNumber = number1;
             number1 = number2;
             number2 = tempNumber;
         }
-        //Crease un bucle for que recorra os numeros que están entre os introducidos
+        //Recorre os numeros que están entre os introducidos
         for (int i = number1; i <= number2; i++) {
-            //Crease unha sentecia if que comprobe se o numero e multiplo de 5
+            //Comproba se o numero e multiplo de 5
             if (i % 5 == 0) {
                 result += i;
             }
