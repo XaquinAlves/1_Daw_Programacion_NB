49516450e28ee784a071e2c2ebcac9395b823d66
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-17 11:25:27 +0200

    Second commit(ComparadorString)

--- actividade2.4.5/src/comparadorstring/ComparadorString.java
+++ actividade2.4.5/src/comparadorstring/ComparadorString.java
@@ -2,6 +2,7 @@ package comparadorstring;
 
 /**
  * Este programa manexa a clase StringComparator creada por nos
+ *
  * @author Xaquin Alves González
  */
 public class ComparadorString {
@@ -10,7 +11,7 @@ public class ComparadorString {
         //Creamos unha nova instancia da clase StringComparator
         StringComparator comparador = new StringComparator("Hola que tal", "Buenas tardes", "Buenos dias");
         //Mostramos por pantalla o método getBigger() para comprobar o funcionamento
-        System.out.println("O texto máis largo é: "+comparador.getBigger());
+        System.out.println("O texto máis largo é: " + comparador.getBigger());
     }
-    
+
 }
--- actividade2.4.5/src/comparadorstring/StringComparator.java
+++ actividade2.4.5/src/comparadorstring/StringComparator.java
@@ -1,28 +1,33 @@
 package comparadorstring;
 
 /**
- * Esta é a clase StringComparator, que posee tres atributos tipo String
+ * Esta é a clase StringComparator, que posee tres atributos tipo String e un 
+ * método que devolve o String mais largo
  * @author Xaquin Alves González
  */
 public class StringComparator {
-    //Creanse as tres variables String coas que traballa esta clase 
+     
     private String chain1;
     private String chain2;
     private String chain3;
     
-    /** Constructor da clase que recibe 3 parametros: 
-     @param chain1 unha cadea de texto calquera
-     @param chain2 unha cadea de texto calquera
-     @param chain3 unha cadea de texto calquera */
+    /** 
+     * Constructor da clase que recibe 3 parametros:      
+     * @param chain1 unha cadea de texto calquera     
+     * @param chain2 unha cadea de texto calquera
+     * @param chain3 unha cadea de texto calquera 
+     */
     public StringComparator(String chain1, String chain2, String chain3) {
         this.chain1 = chain1;
         this.chain2 = chain2;
         this.chain3 = chain3;
     }
     
-    /**Este método compara a lonxitude dos String valendose do metodo length()
+    /**
+     * Este método compara a lonxitude dos String valendose do metodo length()
      * da clase String e unha sentencia if else
-     * @return O String de maior lonxitude dos 3 almacenados */
+     * @return O String de maior lonxitude dos 3 almacenados 
+     */
     public String getBigger(){
         if(chain1.length() > chain2.length() && chain1.length() > chain3.length()){
             return chain1;
