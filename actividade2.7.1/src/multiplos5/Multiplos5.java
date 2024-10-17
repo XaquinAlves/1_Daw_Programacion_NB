23da6e427a9d4762f33b05fc1529ffb61c4d692d
XaquinAlves <xaquinalves@gmail.com>
2024-10-17 12:55:07 +0200

    Second Commit(multiplos5v1)

--- actividade2.7.1/src/multiplos5/Multiplos5.java
+++ actividade2.7.1/src/multiplos5/Multiplos5.java
@@ -3,17 +3,18 @@ package multiplos5;
 /**
  * Este programa calcula a suma dos multiplos de 5 que hay entre dous números
  * @author Xaquin Alves González
+ *
  */
 public class Multiplos5 {
 
     public static void main(String[] args) {
-        //Creanse duas variables int para gardar os numeros e unha para o resultado
+        //Crea dous int para gardar os numeros e un para o resultado
         int number1, number2,result = 0;
         number1 = 7;
         number2 = 33;
-        //Crease un bucle for que recorra os numeros que están entre os introducidos
+        //Recorre os numeros que están entre os introducidos
         for(int i = number1; i <= number2;i++ ){
-            //Crease unha sentecia if que comprobe se o numero e multiplo de 5
+            //Comproba se o numero e multiplo de 5
             if(i % 5 == 0){
                 result += i;
             }
