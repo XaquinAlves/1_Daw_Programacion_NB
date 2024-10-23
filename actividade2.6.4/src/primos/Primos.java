8fa686b2eae577fe60c81ef0f3221d03cec6db26
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-23 16:51:59 +0200

    Fourth Commit (Primos)

--- actividade2.6.4/src/primos/Primos.java
+++ actividade2.6.4/src/primos/Primos.java
@@ -16,33 +16,35 @@ public class Primos {
         int number;
 
         /**
-         * Un bucle do while principal que mantendra o programa en
-         * execución ata que se introduzca 0
+         * Bucle principal que mantendra o programa en execución ata que se
+         * introduzca 0
          */
         do {
-            //Pedimos un número e recollemolo por teclado, gardandoo en number
+            //Pedimos un número por teclado, gardandoo en number
             System.out.println("Introduce un número enteiro(0 para salir): ");
             number = scan.nextInt();
 
-            //Variable que servira de contador para carcular se é primo
+            //Contador 
             int i = 2;
 
             //Comproba se ten divisores a parte de el e a unidade
             while (number % i != 0 && i < number) {
                 i++;
             }
-            
+
             /**
-             * Con esta sentencia if else, comprobamos se a salida do bucle
-             * while anterior foi por atopar un divisor(non primo) ou se chegou
-             * por esgotar as posibilidades(primo)
+             * Comprobamos se a salida do bucle while anterior foi por un
+             * 0(salida do programa), por atopar un divisor(non primo) ou se
+             * chegou por esgotar as posibilidades(primo)
              */
-            if (i == number) {
+            if (number == 0) {
+                System.out.println("Ata a proxima");
+            } else if (i == number) {
                 System.out.println("O número é primo");
             } else {
                 System.out.println("O número non é primo");
             }
-            
+
         } while (number != 0);
     }
 
