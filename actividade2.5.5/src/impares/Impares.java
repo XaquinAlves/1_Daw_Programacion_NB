90d80b64f248c377351c5e2be51557ecdacf9cb5
Xaquin Alves Gonzalez <xaquin.alvesgonzalez@daw1-31.dip.local>
2024-10-30 16:10:26 +0100

    Final commit(Impares)[correxido]

--- actividade2.5.5/src/impares/Impares.java
+++ actividade2.5.5/src/impares/Impares.java
@@ -2,21 +2,18 @@ package impares;
 
 /**
  * Este programa mostra por pantalla os 10 primeiros impares usando un bucle
- * do while e unha sentencia if
+ * for
  * @author Xaquin Alves Gonzalez
  */
 public class Impares {
 
     public static void main(String[] args) {
-        int number=0,i=0;
         
-        do{
-           if(number%2 != 0){
-               System.out.println(number);
-               i++;
-           }
-           number ++;
-        }while(i < 10);
+        for(int i=0, number=1; i<10; i++, number +=2){
+            System.out.println(number);
+        }
+        
+        
     }
     
 }
