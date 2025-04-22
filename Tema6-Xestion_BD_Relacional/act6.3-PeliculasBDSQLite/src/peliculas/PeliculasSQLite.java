c5d38c8c002fe099f08d9ad87f9adcf4de6a10df
XaquinAlves <xaquinalves@gmail.com>
2025-04-22 14:17:59 +0200

    Done actv6.4

--- Tema6-Xestion_BD_Relacional/act6.3-PeliculasBDSQLite/src/peliculas/PeliculasSQLite.java
+++ Tema6-Xestion_BD_Relacional/act6.3-PeliculasBDSQLite/src/peliculas/PeliculasSQLite.java
@@ -1,8 +1,4 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- * 
- * 
  * MIT License
  * 
  * Copyright (c) 2025 Xaquin Alves Gonzalez
